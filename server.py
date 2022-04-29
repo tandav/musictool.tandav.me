93462d8ead48626183104d23572ecbd8f8a0b679
Alexander Rodionov <tandav@tandav.me>
2022-04-29 14:50:38 +0300

    multiclient mvp

--- server.py
+++ server.py
@@ -1,13 +1,16 @@
 import asyncio
 import string
 import random
-from fastapi import FastAPI, WebSocket
-from fastapi.responses import HTMLResponse
-
 import os
-os.environ['MIDI_DEVICE'] = 'IAC Driver Bus 1'
 
 import mido
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
+from musictool.note import SpecificNote
+
+
+os.environ['MIDI_DEVICE'] = 'IAC Driver Bus 1'
+
 port = mido.open_input(os.environ['MIDI_DEVICE'])
 
 app = FastAPI()
@@ -20,6 +23,7 @@ html = """
     </head>
     <body>
         <h1>WebSocket Chat</h1>
+        <h2>Your ID: <span id="ws-id"></span></h2>
         <form action="" onsubmit="sendMessage(event)">
             <input type="text" id="messageText" autocomplete="off"/>
             <button>Send</button>
@@ -27,7 +31,9 @@ html = """
         <ul id='messages'>
         </ul>
         <script>
-            var ws = new WebSocket("ws://localhost:8000/ws");
+            var client_id = Date.now()
+            document.querySelector("#ws-id").textContent = client_id;
+            var ws = new WebSocket(`ws://localhost:8001/ws/${client_id}`);
             ws.onmessage = function(event) {
                 var messages = document.getElementById('messages')
                 var message = document.createElement('li')
@@ -47,17 +53,58 @@ html = """
 """
 
 
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
+
+
 @app.get("/")
 async def get():
     return HTMLResponse(html)
 
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{client_id} left the chat")
 
-    for msg in port:
-        await websocket.send_text(str(msg))
+# @app.websocket("/ws")
+# async def websocket_endpoint(websocket: WebSocket):
+#     await websocket.accept()
+#
+#     try:
+#         # for msg in port.iter_pending():
+#         for msg in port:
+#             if msg.type not in {'note_on', 'note_off'}:
+#                 continue
+#             note = SpecificNote.from_absolute_i(msg.note)
+#             await websocket.send_text(str(note))
+#     except WebSocketDisconnect:
 
     # while True:
         # data = await websocket.receive_text()
